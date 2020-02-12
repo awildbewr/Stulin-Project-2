d0d5d7a0dc87d2bbee0c73de395516e61c791862
Nicholas White <nicholaswhitepersonal@gmail.com>
2020-02-12 12:04:39 +0000

    Added README for user, final stage settings

---
+++ README.md
@@ -0,0 +1,7 @@
+# Stulin-Project-2
+This project is Named: Conway's Game of Life
+-> Takes in an Int from the user that'll determine the size of the 'universe' that user can control.
+
+-> Then takes in a series of Ints to determine which cells are INITIALLY 'alive'.
+
+-> Then evolves the generations however many times the user requests.
--- main.swift
+++ main.swift
@@ -4,15 +4,3 @@ import Foundation
 var vc = Array2DVC()
 
 vc.runLife()
-
-var colony = Colony(size: 10)
-colony.setCellAlive(row: 5, col: 3)
-colony.setCellAlive(row: 5, col: 4)
-colony.setCellAlive(row: 5, col: 5)
-colony.setCellAlive(row: 4, col: 5)
-colony.setCellAlive(row: 3, col: 4)
-print(colony)
-for _ in 0..<12 {
-  colony.evolve()
-  print(colony)
-}
